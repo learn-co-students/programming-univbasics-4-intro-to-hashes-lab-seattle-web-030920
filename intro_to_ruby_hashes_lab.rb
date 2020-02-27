6e964286d081e7e35f5ae2e2a06d3ea26d692512
Joshua Mclean <joshmcleana@gmail.com>
2020-02-27 21:37:58 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,11 +24,12 @@ hash[key]
 end
 
 def update_counting_hash(hash, key)
-if hash[key]
+  if hash[key]
   hash[key] += 1
 else
   hash[key] = 1
-end
+  end
+  return hash
 end
 
 # given a hash an a key as parameters, return an updated hash
